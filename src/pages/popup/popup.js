175f34a4f163c1fbf1f71fb2c4bff2366b653ef0
Jason <jason@Arielles-MacBook-Pro.local>
2024-05-14 14:01:39 -0400

    replacing text completely working now. Must fix api call to deepl

--- src/pages/popup/popup.js
+++ src/pages/popup/popup.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const form = document.getElementById("settings");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const languageValue = form.languages.value;
-  const percentValue = form.range.value;
+  const languageValue = document.getElementById("languages").value;
+  const percentValue = document.getElementById("range").value;
 
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const url = tabs[0].url;
@@ -20,15 +20,22 @@ form.addEventListener("submit", function (event) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         func: async (languageValue, percentValue) => {
-          // console.log(percentValue);
-          // console.log(languageValue);
           chrome.storage.local.get(null, (items) => {
-            // Retrieves all items in storage
-            console.log(items);
-            for (let id in items) {
-              let parentNode = document.getElementById(id);
+            const countToProcess = Math.floor(
+              Object.entries(items).length * (percentValue / 100)
+            );
+            function pickRandomNodes(nodes, count) {
+              if (nodes == {} || nodes === null) return;
+              const shuffled = Object.entries(nodes).sort(
+                () => 0.5 - Math.random()
+              );
+              return shuffled.slice(0, count);
+            }
+            const selectedNodes = pickRandomNodes(items, countToProcess);
+
+            selectedNodes.map((node) => {
+              let parentNode = document.getElementById(node[0]);
               if (parentNode) {
-                // Assuming only one text node per parent, which is a simplification
                 let textNode = [...parentNode.childNodes].find(
                   (node) => node.nodeType === Node.TEXT_NODE
                 );
@@ -39,27 +46,42 @@ form.addEventListener("submit", function (event) {
                   );
                 }
               }
-            }
+            });
+            // for (let id in selectedNodes) {
+            //   debugger;
+            //   let parentNode = document.getElementById(id);
+            //   if (parentNode) {
+            //     let textNode = [...parentNode.childNodes].find(
+            //       (node) => node.nodeType === Node.TEXT_NODE
+            //     );
+            //     if (textNode) {
+            //       textNode.nodeValue = textNode.nodeValue.replace(
+            //         textNode.nodeValue,
+            //         "replacement"
+            //       );
+            //     }
+            //   }
+            // }
           });
 
-          let allWords = [];
-          nodes.forEach((node) => {
-            let words = node.text.split(/\s+/);
-            allWords = allWords.concat(words.map((word) => ({ word, node })));
-          });
-          let selectionCount = Math.floor(allWords.length * (100 / 100));
-          let selectedWords = [];
-          while (selectedWords.length < selectionCount) {
-            let index = Math.floor(Math.random() * allWords.length);
-            selectedWords.push(allWords.splice(index, 1)[0]);
-          }
+          // let allWords = [];
+          // nodes.forEach((node) => {
+          //   let words = node.text.split(/\s+/);
+          //   allWords = allWords.concat(words.map((word) => ({ word, node })));
+          // });
+          // let selectionCount = Math.floor(allWords.length * (100 / 100));
+          // let selectedWords = [];
+          // while (selectedWords.length < selectionCount) {
+          //   let index = Math.floor(Math.random() * allWords.length);
+          //   selectedWords.push(allWords.splice(index, 1)[0]);
+          // }
           // return selectedWords;
           // console.log(selectedWords);
-          selectedWords.forEach((entry, index) => {
-            let originalText = entry.node.text;
-            // let translatedWord = translations[index];
-            entry.node.text = originalText.replace(entry.word, "test");
-          });
+          // selectedWords.forEach((entry, index) => {
+          //   let originalText = entry.node.text;
+          //   // let translatedWord = translations[index];
+          //   entry.node.text = originalText.replace(entry.word, "test");
+          // });
           chrome.storage.local.clear(() => {
             if (chrome.runtime.lastError) {
               console.error(
@@ -98,7 +120,7 @@ form.addEventListener("submit", function (event) {
           //   return null;
           // }
         },
-        args: [percentValue, languageValue]
+        args: [languageValue, percentValue]
       });
     } else {
       console.error("Cannot execute script on chrome:// URLs");
