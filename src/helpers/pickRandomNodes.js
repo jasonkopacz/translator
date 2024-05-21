7a6edd898c840ad8ef451a4c7ec93d4c60725c42
Jason <jason@Arielles-MacBook-Pro.local>
2024-05-21 17:47:28 -0400

    migrated content to its own folder

--- src/helpers/pickRandomNodes.js
+++
@@ -1,8 +0,0 @@
-function pickRandomNodes(nodes, count) {
-  if (nodes == {} || nodes === null) return [];
-  const shuffled = Object.entries(nodes).sort(() => 0.5 - Math.random());
-  const fullNodes = shuffled.slice(0, count);
-  const textNodes = fullNodes.map((text) => text[1]);
-  return { fullNodes, textNodes };
-}
-export default pickRandomNodes;
